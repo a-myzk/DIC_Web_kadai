3929f03d80d263e001675f236a32a7ff7c1d2d04
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-03-08 19:09:55 +0900

    Web技術入門課題

---
+++ test.rb
@@ -0,0 +1,16 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+# server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+# server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'kadai.html.erb')
+server.mount('/kadai_goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'kadai_goya.rb')
+server.mount('/kadai_goya_quality.cgi', WEBrick::HTTPServlet::CGIHandler, 'kadai_goya_quality.rb')
+server.start
